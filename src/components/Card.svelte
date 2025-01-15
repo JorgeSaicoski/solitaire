63a32b2027257cd563554f5105e04e1483e573ee
JorgeSaicoski <jorgesaicoski@gmail.com>
2025-01-15 12:13:34 -0300

    flip card

---
+++ src/components/Card.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let rank = ''; // Card rank (e.g., 'A', '2', '3', ..., 'K')
+  export let suit = ''; // Card suit (e.g., 'hearts', 'diamonds', 'clubs', 'spades')
+  export let isFaceUp = false; // Whether the card is face-up or face-down
+  function flipCard() {
+    isFaceUp = !isFaceUp;
+  }
+</script>
+
+<button
+    onclick={()=>flipCard()}
+  class="w-20 h-32 border rounded-lg flex justify-center items-center bg-white shadow-lg transition-all duration-300 ease-in-out"
+  class:flip={isFaceUp} 
+  class:bg-gray-200={!isFaceUp}
+  class:border-gray-700={!isFaceUp}
+>
+  {#if isFaceUp}
+    <div class="bg-blue-500 w-full h-full rounded-lg"></div>
+  {:else}   
+    <div class="text-xl font-bold">
+        <span>{rank}</span>
+        <span class="block text-xs">{suit}</span>
+      </div>
+  {/if}
+</button>
+
+<style>
+  .flip {
+    transform: rotateY(180deg);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Card from '../components/Card.svelte';
+</script>
+
+<main class="flex justify-center items-center h-screen bg-gray-100">
+  <Card rank="A" suit='hearts'/>
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+  }
+</style>
